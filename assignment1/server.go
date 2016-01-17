979431d35cd3b7a61b5c9464014afd814e0a5c80
Aakash Deshpande <Aakash@Aakashs-MacBook-Pro.local>
2016-01-17 14:15:15 +0530

    File server with coarse locking

--- assignment1/server.go
+++ assignment1/server.go
@@ -3,157 +3,353 @@ package main
 import "fmt"
 import "net"
 import "io"
-//import "io/ioutil"
 import "os"
 import "strings"
 import "strconv"
 import "github.com/syndtr/goleveldb/leveldb"
+import "sync"
+import "time"
 //import "reflect"
+//import "io/ioutil"
 
 func serverMain() {
-  db, _ := leveldb.OpenFile("$GOPATH/src/github.com/aakashdeshpande/cs733/assignment1/versionMap", nil)
-  defer db.Close()
+    // Database to store file version   
+    db, _ := leveldb.OpenFile("$GOPATH/src/github.com/aakashdeshpande/cs733/assignment1/versionMap", nil)
+    defer db.Close()
+
+    var mutex = &sync.Mutex{}
   
-	ln, err := net.Listen("tcp", ":8080")
+	// Listen for TCP connection on port 8080
+    ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		// handle error
 		fmt.Println("Error listening:", err.Error())
         os.Exit(1)
 	}
   
+    // Keep listening for new connections
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			// handle error
 			fmt.Println("Error accepting: ", err.Error())
             os.Exit(1)
 		}
-		go handleConnection(conn, db)
+        // Handle new connection in a new thread
+		go handleConnection(conn, db, mutex)
 	}
   
 }
 
-func handleConnection(conn net.Conn, db *leveldb.DB) {
-  
-    defer conn.Close()
-    // Make a buffer to hold incoming data.
-    var bufferSize int64 = 32
+func handleConnection(conn net.Conn, db *leveldb.DB, mutex *sync.Mutex) {
+    
+    defer conn.Close() // make sure to close the connection even if we panic.
+    // Make a buffer to hold incoming data with size bufferSize
+    var bufferSize int64 = 1024
     buf := make([]byte, bufferSize)
-    //var terminated bool
     var fileName string 
+    
+    // Initial command string and number of bytes read with the command
+    var sLarge string = ""
+    var bufferRead int = 0
 
-    // Read the incoming connection into the buffer.
-    // Warning : We might not get the complete message
-    n, err := conn.Read(buf)
-    // Was there an error in reading ?
-    if err != nil {
-        if err != io.EOF {
-            fmt.Println("Error reading:", err.Error())
+    OUTER:    
+    for{
+
+        buf = []byte(sLarge)   
+        bufferRead = len([]byte(sLarge)) 
+
+        // Read the incoming connection into the buffer
+        // Warning : We might not get the complete message
+        for{
+            if strings.Contains(sLarge, "\r\n") || len(sLarge) > 100000{
+                break
+            }
+            buf = make([]byte, bufferSize)
+            n, err := conn.Read(buf)
+            // Was there an error in reading ?
+            if err != nil {
+                if err != io.EOF {
+                    fmt.Println("Error reading:", err.Error())
+                }
+            }
+            sLarge += string(buf[:n])
+            bufferRead = n
         }
-        //break
-    }
+        
+        fmt.Println("Reached")
+        s := strings.Split(sLarge, "\r\n") // Isolate main command from input 
+        commands := strings.Split(s[0], " ")
+        // Remove extra input apart from basic command string
+        sLarge = string(buf[int64(len([]byte(s[0] + "\r\n"))):bufferRead])
 
-    sLarge := string(buf[:n])
-    s := strings.Split(sLarge, "\r\n")
-    commands := strings.Split(s[0], " ")
-
-    if(len(commands) < 2){
-        conn.Write([]byte("ERR_CMD_ERR\r\n"))
-    }else { 
-        fileName = commands[1]
-    }
-
-    if commands[0] == "read" {
-        file, err := os.Open(fileName) // For read access.
-        if err != nil {
-            conn.Write([]byte("ERR_FILE_NOT_FOUND\r\n"))
-            return
+        if(len(commands) < 2){
+            conn.Write([]byte("ERR_CMD_ERR\r\n"))
+            continue OUTER
+        }else { 
+            fileName = commands[1]
         }
-        defer file.Close() // make sure to close the file even if we panic.
-        _, err = io.Copy(conn, file)
-        if err != nil {
-            conn.Write([]byte("ERR_INTERNAL\r\n"))
-        } else {
+
+        if commands[0] == "read" {
+            file, err := os.Open(fileName) // For read access.
+            if err != nil {
+                conn.Write([]byte("ERR_FILE_NOT_FOUND\r\n"))
+                return
+            }
+            defer file.Close() // make sure to close the file even if we panic.
+
+            //_, err = io.Copy(conn, file)
+
+            var currentByte int64 = 0
+            fileBuffer := make([]byte, bufferSize)
+
+            //read file until there is an error
+            for {
+                n, err := file.ReadAt(fileBuffer, currentByte)
+                currentByte += int64(n)
+                //fmt.Println(fileBuffer) //This is just for checking
+                conn.Write(fileBuffer[:n])
+                if err == io.EOF {
+                    break
+                }
+                if err != nil && err != io.EOF {
+                    conn.Write([]byte("ERR_INTERNAL\r\n"))
+                    break
+                } 
+            }
+
+            if err != nil && err != io.EOF {
+                conn.Write([]byte("ERR_INTERNAL\r\n"))
+            } else { 
+                var version int64 = 0
+                data, err := db.Get([]byte(fileName), nil)
+                if err == nil {
+                    version, err = strconv.ParseInt(string(data), 10, 64)
+                }
+                conn.Write([]byte("OK " + strconv.FormatInt(version,10) + "\r\n"))
+            }
+        } else if commands[0] == "delete" {
+            err := os.Remove(fileName)
+            if err != nil {
+                conn.Write([]byte("ERR_FILE_NOT_FOUND\r\n"))
+            } else {
+                db.Delete([]byte(fileName), nil)
+                conn.Write([]byte("OK \r\n"))
+            }
+        } else if commands[0] == "write" {
+            sLarge = ""
+
+            if(len(commands) < 3){
+                conn.Write([]byte("ERR_CMD_ERR\r\n"))
+                continue OUTER
+            }
+
+            file, err := os.Create(fileName) // For write access or to create the file otherwise.
+            if err != nil {
+                conn.Write([]byte("ERR_INTERNAL\r\n"))
+                fmt.Println(err)
+                return
+            }
+            defer file.Close() // make sure to close the file even if we panic.
+
+            fileSize, err := strconv.ParseInt(commands[2], 10, 64)
+            fileSize = fileSize + int64(len([]byte("\r\n")))
+
+            mutex.Lock()
+
+            var currentByte int64 = 0
+            // If we have already read part of the file along with the command
+            if len(s) > 1{
+                bufSplit := buf[len([]byte(s[0] + "\r\n")):bufferRead]
+                if int64(len(bufSplit)) > fileSize {
+                    sLarge = string(buf[int64(len([]byte(s[0] + "\r\n"))) + fileSize:bufferRead])
+                    bufSplit = bufSplit[:fileSize]
+                }
+                _, err := file.WriteAt(bufSplit, currentByte)
+                currentByte += int64(bufferRead - len([]byte(s[0] + "\r\n")))
+                if err != nil {
+                    conn.Write([]byte("ERR_INTERNAL\r\n"))
+                    return
+                }
+            }
+
+            c1 := make(chan bool, 1) 
+            if len(commands) > 3 {
+                exp, _  := strconv.Atoi(commands[3])
+                if exp > 0{
+                    go func(exp int) {
+                        time.Sleep(time.Second * time.Duration(exp))
+                        c1 <- true
+                    }(exp)
+                }
+            }
+
+            var res bool = false 
+            var n int = 0
+            // Keep reading connection until file bytes are read
+            for {
+                if currentByte >= fileSize {
+                    break
+                }
+                buf = make([]byte, bufferSize)
+                
+                go func(n *int, conn net.Conn, buf *[]byte) {
+                    *n, _ = conn.Read(*buf)
+                    c1 <- false
+                }(&n, conn, &buf)
+                
+                res = <- c1
+                if res == true {
+                    break
+                }
+
+                if int64(n) > fileSize - currentByte {
+                    bufSplit := buf[:fileSize - currentByte]
+                    _, err = file.WriteAt(bufSplit, currentByte)
+                } else {
+                    _, err = file.WriteAt(buf, currentByte)
+                }
+                if err != nil {
+                    conn.Write([]byte("ERR_INTERNAL\r\n"))
+                    return
+                }
+                if currentByte + int64(n) > fileSize {
+                    sLarge = string(buf[fileSize - currentByte:])
+                }
+                currentByte += int64(n)    
+            }
+
+            // Get file version
             var version int64 = 0
             data, err := db.Get([]byte(fileName), nil)
             if err == nil {
                 version, err = strconv.ParseInt(string(data), 10, 64)
             }
-            conn.Write([]byte("OK " + strconv.FormatInt(version+1,10) + "\r\n"))
-        }
-    } else if commands[0] == "delete" {
-        err := os.Remove(fileName)
-        if err != nil {
-            conn.Write([]byte("ERR_FILE_NOT_FOUND\r\n"))
+
+            err = db.Put([]byte(fileName), []byte(strconv.FormatInt(version+1, 10)), nil)
+            conn.Write([]byte("OK " + strconv.FormatInt(version+1, 10) + "\r\n"))    
+            mutex.Unlock()
+
+            if res == true {
+                <- c1
+                sLarge = string(buf[:n])
+            }
+        } else if commands[0] == "cas" {
+            sLarge = ""
+
+            if(len(commands) < 4){
+                conn.Write([]byte("ERR_CMD_ERR\r\n"))
+                continue OUTER
+            } 
+
+            mutex.Lock()
+            var version int64 = 0
+            data, err := db.Get([]byte(fileName), nil)
+            if err == nil {
+                version, err = strconv.ParseInt(string(data), 10, 64)
+            }
+
+            fileVersion, err := strconv.ParseInt(commands[2], 10, 64)
+
+            if err != nil {
+                conn.Write([]byte("ERR_INTERNAL\r\n"))
+                fmt.Println(err)
+                return
+            }
+
+            if version != fileVersion {
+                conn.Write([]byte("ERR_VERSION\r\n"))
+                fmt.Println(err)
+                mutex.Unlock()
+                continue OUTER
+            }        
+
+            file, err := os.Create(fileName)
+            defer file.Close() // make sure to close the file even if we panic.
+            if err != nil {
+                conn.Write([]byte("ERR_INTERNAL\r\n"))
+                fmt.Println(err)
+                return
+            }
+
+            fileSize, err := strconv.ParseInt(commands[3], 10, 64)
+            fileSize = fileSize + int64(len([]byte("\r\n")))
+
+            var currentByte int64 = 0
+            // If we have already read part of the file along with the command
+            if len(s) > 1{
+                bufSplit := buf[len([]byte(s[0] + "\r\n")):bufferRead]
+                if int64(len(bufSplit)) > fileSize {
+                    sLarge = string(buf[int64(len([]byte(s[0] + "\r\n"))) + fileSize:bufferRead])
+                    bufSplit = bufSplit[:fileSize]
+                }
+                _, err := file.WriteAt(bufSplit, currentByte)
+                currentByte += int64(bufferRead - len([]byte(s[0] + "\r\n")))
+                if err != nil {
+                    conn.Write([]byte("ERR_INTERNAL\r\n"))
+                    return
+                }
+            }
+
+            c1 := make(chan bool, 1) 
+            if len(commands) > 4 {
+                exp, _  := strconv.Atoi(commands[4])
+                if exp > 0{
+                    go func(exp int) {
+                        time.Sleep(time.Second * time.Duration(exp))
+                        c1 <- true
+                    }(exp)
+                }
+            }
+
+            var res bool = false 
+            var n int = 0
+            // Keep reading connection until file bytes are read
+            for {
+                if currentByte >= fileSize {
+                    break
+                }
+                buf = make([]byte, bufferSize)
+                
+                go func(n *int, conn net.Conn, buf *[]byte) {
+                    *n, _ = conn.Read(*buf)
+                    c1 <- false
+                }(&n, conn, &buf)
+                
+                res = <- c1
+                if res == true {
+                    break
+                }
+
+                if int64(n) > fileSize - currentByte {
+                    bufSplit := buf[:fileSize - currentByte]
+                    _, err = file.WriteAt(bufSplit, currentByte)
+                } else {
+                    _, err = file.WriteAt(buf, currentByte)
+                }
+                if err != nil {
+                    conn.Write([]byte("ERR_INTERNAL\r\n"))
+                    return
+                }
+                if currentByte + int64(n) > fileSize {
+                    sLarge = string(buf[fileSize - currentByte:])
+                }
+                currentByte += int64(n)    
+            }
+
+            file.Sync() // Check
+            err = db.Put([]byte(fileName), []byte(strconv.FormatInt(version, 10)), nil)
+            conn.Write([]byte("OK " + strconv.FormatInt(version, 10) + "\r\n"))
+            mutex.Unlock()
+
+            if res == true {
+                <- c1
+                sLarge = string(buf[:n])
+            }
         } else {
-            err := db.Delete([]byte(fileName), nil)
-            if err != nil {
-                conn.Write([]byte("ERR_FILE_NOT_FOUND\r\n"))
-            } else {
-                conn.Write([]byte("OK \r\n"))
-            }
-        }
-    } else if commands[0] == "write" {
-        var version int64 = 0
-        _, err := os.Stat(fileName); 
-        data, err := db.Get([]byte(fileName), nil)
-        if err == nil {
-            version, err = strconv.ParseInt(string(data), 10, 64)
+            conn.Write([]byte("ERR_CMD_ERR\r\n"))
+            continue OUTER
         }
 
-        file, err := os.Create(fileName)
-        defer file.Close() // make sure to close the file even if we panic.
-        if err != nil {
-            conn.Write([]byte("ERR_INTERNAL\r\n"))
-            fmt.Println(err)
-            return
-        }
-
-        fileSize, err := strconv.ParseInt(commands[2], 10, 64)
-
-        var currentByte int64 = 0
-        if len(s) > 1{
-            bufSplit := buf[len([]byte(s[0] + "\r\n")):n]
-            if int64(len(bufSplit)) > fileSize {
-                bufSplit = bufSplit[:fileSize]
-            }
-            _, err := file.WriteAt(bufSplit, currentByte)
-            currentByte += int64(len(bufSplit))
-            if err != nil {
-                conn.Write([]byte("ERR_INTERNAL\r\n"))
-                return
-            }
-        }
-
-        for {
-            if currentByte >= int64(fileSize) {
-                break
-            }
-            n, err := conn.Read(buf)
-            if int64(n) > fileSize - currentByte {
-                buf = buf[:fileSize - currentByte]
-            }
-            _, err = file.WriteAt(buf, currentByte)
-            currentByte += int64(n)
-            if err != nil {
-                conn.Write([]byte("ERR_INTERNAL\r\n"))
-                return
-            }
-        }
-
-        file.Sync()
-        //versionMap[fileName]++
-        err = db.Put([]byte(fileName), []byte(strconv.FormatInt(version+1, 10)), nil)
-        conn.Write([]byte("OK " + strconv.FormatInt(version+1, 10) + "\r\n"))
-    } else if commands[0] == "cas" {
-        d2 := []byte(s[1])
-        f, _ := os.Create("/Users/Aakash/Documents/" + fileName)
-        f.Write(d2)
-        f.Sync()
-    } else {
-        conn.Write([]byte("ERR_CMD_ERR\r\n"))
     }
-
 }
 
 
--- assignment1/server_test.go
+++ assignment1/server_test.go
@@ -2,6 +2,7 @@ package main
 import "net" 
 import "fmt" 
 import "bufio" 
+//import "io"
 //import "os"
 
 import (
@@ -22,25 +23,35 @@ func TestServer(t *testing.T) {
 	//for {     
 		// read in input from stdin     
 		//reader := bufio.NewReader(os.Stdin)     
-		text, _ := "write input2.txt 50\r\nThis is serious xyzsgydflysebfkzrmgldkmgiov;SHDUYWfstyawdsahbfduds;hgoidfjmgkdfmglksbdkuyafwdytavdhvawlhbdunThere are two types of assignment operators in go := and =. They are semantically equivalent (with respect to assignment) but the first one is also a ( http://golang.org/ref/spec#Short_variable_declarations ) which means that in the left we need to have at least a new variable declaration for it to be correct. \r", " " //reader.ReadString('\n')     
+		text, _ := "write input2.txt 50\r\nThis is serious. There are two types of assignment operators in go := and =. They are semantically equivalent (with respect to assignment) but the first one is also a ( http://golang.org/ref/spec#Short_variable_declarations ) which means that in the left we need to have at least a new variable declaration for it to be correct. \r", " " //reader.ReadString('\n')     
 		// send to socket     
 		fmt.Fprintf(conn, text + "\n")
 
-		message, _ := bufio.NewReader(conn).ReadString('\n')     
-		fmt.Print("Message from server: "+message)   
 	//} 
 
 		// connect to this socket   
-	conn2, _ := net.Dial("tcp", "localhost:8080")  
+		conn2, _ := net.Dial("tcp", "localhost:8080")  
 
 	//for {     
 		// read in input from stdin     
 		//reader := bufio.NewReader(os.Stdin)     
-		text2, _ := "write input2.txt 50\r\nThis is serious too xyzsgydflysebfkzrmgldkmgiov;SHDUYWfstyawdsahbfduds;hgoidfjmgkdfmglksbdkuyafwdytavdhvawlhbdunThere are two types of assignment operators in go := and =. They are semantically equivalent (with respect to assignment) but the first one is also a ( http://golang.org/ref/spec#Short_variable_declarations ) which means that in the left we need to have at least a new variable declaration for it to be correct. \r", " " //reader.ReadString('\n')     
+		text2, _ := "write input2.txt 10\r\nThis\r\n is:\r\nread input2.txt\r", " " //reader.ReadString('\n')     
 		// send to socket     
 		fmt.Fprintf(conn2, text2 + "\n")
 
-		message2, _ := bufio.NewReader(conn2).ReadString('\n')     
-		fmt.Print("Message from server: "+message2)   
+		message, _ := bufio.NewReader(conn).ReadString('\n')     
+		fmt.Print("Message from server: "+message)   
+
+		message, _ = bufio.NewReader(conn2).ReadString('\n')     
+		fmt.Print("Message from server: "+message)  
+
+		message, _ = bufio.NewReader(conn2).ReadString(':')     
+		fmt.Print("Message from server: "+message)  
+
+				message, _ = bufio.NewReader(conn2).ReadString('\n')     
+		fmt.Print("Message from server: "+message)  
+
+  
+
 	//} 	
 } 
